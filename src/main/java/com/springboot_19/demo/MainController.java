a0cfd1115417521b3608661c3b92e4814515eb92
natashalballard <nbitcareer@gmail.com>
2018-02-12 10:30:16 -0500

    Initial commit

---
+++ src/main/java/com/springboot_19/demo/MainController.java
@@ -0,0 +1,25 @@
+package com.springboot_19.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+}
